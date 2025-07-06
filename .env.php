5ea71caa6ab11ff0625b3ca53c201f3bdc1da575
NTIC Solution <nticsolution.bf@gmail.com>
2025-07-06 13:06:11 +0000

    Create .env.php

---
+++ .env.php
@@ -0,0 +1,8 @@
+<?php
+// ⚠️ Ne jamais pousser ce fichier publiquement sur GitHub
+
+define('DB_HOST', 'sql100.infinityfree.com');
+define('DB_USER', 'if0_39369296');
+define('DB_PASS', '67222863L');  // remplacez avec le vrai mot de passe
+define('DB_NAME', 'if0_39369296_deal');        // remplacez XXX par le nom réel de la base
+?>
